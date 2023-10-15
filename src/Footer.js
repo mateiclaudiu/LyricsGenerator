a43e944cf1567b3a6865d76c125a86e59a149e44
claudiu <claudiu@matei.be>
2023-10-15 20:12:01 +0200

    reframe footer text with matei as copyrighter

--- src/Footer.js
+++ src/Footer.js
@@ -5,10 +5,10 @@ import './Footer.css'; // Make sure the path is correct based on your project st
 const Footer = () => {
     return (
         <footer className="App-footer">
-            <p>© {new Date().getFullYear()} <a href="https://www.elimantwerpen.be/" target="_blank" rel="noopener noreferrer">Biserica Elim Antwerpen</a></p>
+            <p><a href="https://www.elimantwerpen.be/" target="_blank" rel="noopener noreferrer">Biserica Elim Antwerpen</a></p>
             <p>Pastoor Campensstraat 4, 2170 Antwerpen, Belgia</p>
             <p>Duminica ora 11.00-13.00</p>
-            <p>creat de <a href="https://www.matei.be/" target="_blank" rel="noopener noreferrer">matei</a></p>
+            <p>© {new Date().getFullYear()} <a href="https://www.matei.be/" target="_blank" rel="noopener noreferrer">matei</a></p>
             {/* This ensures that the link opens in a new tab and is secure */}
         </footer>
     );
